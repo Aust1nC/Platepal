69ef37e3712dce86c020b452eb1f172d2c7c52c9
Austin <austinchen_zy@hotmail.com>
2024-07-24 15:58:07 +0100

    First commit - initialize project

---
+++ src/test/java/com/austin/platpal_ai_backend/PlatpalAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austin.platpal_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatpalAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
